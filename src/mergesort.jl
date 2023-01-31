fc588f8600e6b5c6849d909a0f5b6fd36deb2d17
codegodz <biobeeloo@gmail.com>
2023-01-31 09:39:10 +0100

    fix type

--- src/mergesort.jl
+++ src/mergesort.jl
@@ -58,7 +58,7 @@ function kway_heap_merge(vecs::Union{AbstractVector{Vector{T}}, AbstractVector{D
 end
 
 # Overload of method below to also directly allow passing mmaps
-function kway_mmap_merge(maps::Vector{AbstractVector}, output_file::String, type::T ; use_heap::Bool = false) where T <:DataType
+function kway_mmap_merge(maps::AbstractVector{Vector{Int64}}, output_file::String, type::T ; use_heap::Bool = false) where T <:DataType
     # We calculate the mmap size based on the provided type variable
     total_size = sum(map(length, maps))
     # Allocate the output size
